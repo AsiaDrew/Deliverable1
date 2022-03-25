ffdc179620ca614a8cb2f10dab06c435164c4bcf
AsiaDrew <asiadrew2@outlook.com>
2022-03-25 17:59:08 -0400

    First commit for deliverable one

--- Deliverable1/Deliverable1/Program.cs
+++ Deliverable1/Deliverable1/Program.cs
@@ -6,7 +6,79 @@ namespace Deliverable1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool continueQuestion = true;
+            while (continueQuestion)
+            {
+                Console.WriteLine();
+                Console.WriteLine("How many people are we making PB&J sandwiches for?");
+                int userinput = int.Parse(Console.ReadLine());
+
+                int bread = 2;
+                int butter = 2;
+                int jelly = 4;
+                int jarbutter = 32;
+                int jarjelly = 48;
+                double totalslicesneeded = 2 * userinput;
+                double slicesperloaf = 28;
+                double totalbutterneeded = 2 * userinput;
+                double totaljellyneeded = 4 * userinput;
+
+                double totalloafneeded = Math.Round(totalslicesneeded / slicesperloaf);
+                double totalbutter = Math.Round(totalbutterneeded / jarbutter);
+                double totaljelly = Math.Round(totaljellyneeded / jarjelly);
+
+                {
+                    string message = "You need:";
+                    Console.WriteLine();
+                    Console.WriteLine(message);
+                    Console.WriteLine();
+                }
+                {
+                    Console.WriteLine((bread * userinput) + " Slices of Bread");
+                    Console.WriteLine((butter * userinput) + " Tablespoons of Peanut Butter");
+                    Console.WriteLine((jelly * userinput) + " Teaspoons of Jelly");
+                    Console.WriteLine();
+                    {
+                        Console.WriteLine("Which is:");
+                        Console.WriteLine();
+                        Console.WriteLine(totalloafneeded + " Loaves of Bread");
+                        Console.WriteLine(totalbutter + " Jars of Peanut Butter");
+                        Console.WriteLine(totaljelly + " Jars of Jelly");
+                        Console.WriteLine();
+
+                        Console.WriteLine("Would you like to restart? Enter yes or y to continue, or enter any other key to exit.");
+                        string input = (Console.ReadLine());
+
+                        if (input.ToLower() == "yes" || input.ToLower() == "y")
+                        {
+                            
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.Write("GoodBye!!!");
+                            continueQuestion = false;
+                            break;
+
+                        }
+                    }
+
+                }
+
+            }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
